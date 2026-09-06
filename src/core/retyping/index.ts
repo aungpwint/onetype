e50b5b102428369990f9283144952bf034596726
Aung Pwint <aungpwintofficial@gmail.com>
2026-09-06 10:26:49 +0630

    refactor: remove obsolete retyping module and its tests

--- src/core/retyping/index.ts
+++
@@ -1,2 +0,0 @@
-export { RetypeEngine, retypeMatch } from './retype-engine'
-export type { RetypeOptions, RetypeEngineOptions, RetypeComparisonPolicy, RetypeGraphemeResult, RetypeProgress, GraphemeState } from './retype-engine'
